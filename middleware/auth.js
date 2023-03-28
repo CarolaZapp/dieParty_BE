419dfa62632382d50450807943feaca3049393cf
Carola Zapp <carola.zapp@gmx.net>
2023-03-28 09:14:36 +0200

    one console.log commented out

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ const JWT_KEY = process.env.SECRET_JWT_KEY || "Default";
 const auth = (req, res, next) => {
   try {
     const token = req.cookies.loginCookie;
-    console.log(token);
+    // console.log(token);
     const decodedToken = jwt.verify(token, JWT_KEY);
     console.log("User ist verifiziert, Daten werden an den Client gesendet");
     req.token = decodedToken;
