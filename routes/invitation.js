c6472635450bf2dfeb08de28de274cc70f2b8d5c
Carola Zapp <carola.zapp@gmx.net>
2023-04-03 15:03:46 +0200

    2x get

--- routes/invitation.js
+++ routes/invitation.js
@@ -4,15 +4,15 @@ import auth from "../middleware/auth.js";
 
 // import controller functions
 import {
-  // getInvitation,
+  getInvitation,
   getUserInvitation, // Idee 
   postOneInvitation,
 } from "../controller/invitationController.js";
 
 // Routes
 // router.route("/").get(auth, getInvitation).post(auth, postOneInvitation);
-// router.route("/").get(getInvitation).post(auth, postOneInvitation);
-router.route("/").post(auth, postOneInvitation);
+router.route("/").get(getInvitation).post(auth, postOneInvitation);
+// router.route("/").post(auth, postOneInvitation);
 router.route("/:eventId").get(getUserInvitation);
 
 
